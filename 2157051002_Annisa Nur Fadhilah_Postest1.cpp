133b1bc89b657acd3309f10b54c9c4353fd3ba4e
annisa <92286817+fdhannisa@users.noreply.github.com>
2021-11-23 11:25:23 +0700

    Add files via upload

---
+++ 2157051002_Annisa Nur Fadhilah_Postest1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<" Post test"<<endl;
+	cout<<" Annisa Nur Fadhilah"<<endl;
+	cout<<" NPM: 2157051002"<<endl;
+	
+	cout<<"==================================="<<endl;
+	
+	string a, b, c;
+	
+	cout<<" Persamaan kuadrat ax^2 + bx + c";
+	cout<<" Masukan nilai"<<endl;
+	cout<<" a, b, c ="<<endl;
+	cin>> a >> b >> c;
+	
+	cout<<" Hasil dari persamaan"<<endl;
+	cout<< a <<" x^2 +"<< b <<"x +"<< c;
+	
+	return 0;
+}
